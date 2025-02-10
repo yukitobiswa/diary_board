15a6958f6309824b9c22b7080fe893332cfc0fbd
tobi-sawa <is0680xr@ed.ritsumei.ac.jp>
2025-02-10 09:22:32 +0900

    a

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ from BM import (
 
 
 # Database URL
-DATABASE_URL = "mysql+pymysql://root:6213ryoy@127.0.0.1/demo"
+DATABASE_URL = "mysql+pymysql://root:yuki108@127.0.0.1/demo"
 # FastAPI app
 app = FastAPI()
 logger = logging.getLogger(__name__)
